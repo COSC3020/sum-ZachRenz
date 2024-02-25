6c6a85fd1db9597bfbba6a2e5014b26de89d5791
ZachRenz <zachary.ginger539@gmail.com>
2024-02-25 00:07:34 -0700

    test

--- code.js
+++ code.js
@@ -10,3 +10,4 @@ function sum(a) {
 // For some odd reason, I kept getting error messages when I used var for my variables, when I swtiched to let to create variables in 
 // this function block it worked. I'm not sure why or if that was something you wanted us to catch, but I figrued I would let you know.
 // I'm using vs to edit my code but node.js to run it in powershell, I'm on windows. 
+
